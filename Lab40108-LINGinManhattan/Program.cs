682b84475c9ef564d404852ad041294b1ece2c99
Andrew Hinojosa <drewsview34@gmail.com>
2019-01-17 17:39:26 -0800

    Add project files.

---
+++ Lab40108-LINGinManhattan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab40108_LINGinManhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
